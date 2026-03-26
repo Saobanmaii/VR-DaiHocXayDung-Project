1a4faa83ae1d34eea73d759bd99c8b98a6a92560
hieutrinh1610 <fanmarvel1610@gmail.com>
2026-03-26 23:54:49 +0700

    thêm pha 2.2
    warning rào chắn

--- VR Room/Assets/_Course Library/Hieu/Warning.cs
+++ VR Room/Assets/_Course Library/Hieu/Warning.cs
@@ -2,9 +2,10 @@
 
 public class HienThiUIDonGian : MonoBehaviour
 {
-    [Header("Kéo thả UI và Âm thanh vào đây")]
+    [Header("Kéo thả UI, Âm thanh và Rào chắn vào đây")]
     public GameObject uiCanhBao;   // Kéo cụm UI vào đây
     public AudioSource coiBaoDong; // Kéo vật chứa còi vào đây
+    public GameObject raoChan;     // Kéo vật thể Rào chắn (bức tường lưới mờ) vào đây
 
     void Start()
     {
@@ -12,6 +13,12 @@ public class HienThiUIDonGian : MonoBehaviour
         if (uiCanhBao != null)
         {
             uiCanhBao.SetActive(false);
+        }
+
+        // Ẩn Rào chắn lúc mới bắt đầu game
+        if (raoChan != null)
+        {
+            raoChan.SetActive(false);
         }
     }
 
@@ -23,6 +30,9 @@ public class HienThiUIDonGian : MonoBehaviour
 
         // Bật còi (nếu chưa kêu)
         if (coiBaoDong != null && !coiBaoDong.isPlaying) coiBaoDong.Play();
+
+        // Hiện Rào chắn lên
+        if (raoChan != null) raoChan.SetActive(true);
     }
 
     // BẤT CỨ VẬT GÌ đi ra khỏi Trigger đều chạy hàm này
@@ -33,5 +43,8 @@ public class HienThiUIDonGian : MonoBehaviour
 
         // Tắt còi
         if (coiBaoDong != null) coiBaoDong.Stop();
+
+        // CỐ TÌNH KHÔNG LÀM GÌ VỚI RÀO CHẮN Ở ĐÂY ĐỂ NÓ HIỆN VĨNH VIỄN
+        if (raoChan != null) raoChan.SetActive(false);
     }
 }
