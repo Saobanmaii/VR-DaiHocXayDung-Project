7246e8958e2f10246ac222b98c28f7e23784cd61
hieutrinh1610 <fanmarvel1610@gmail.com>
2026-05-15 00:46:11 +0700

    update scene intro
    Thêm scene hướng dẫn,hoàn thiện

---
+++ Assets/Hieu/Code_Hieu/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Bắt buộc phải khai báo thư viện này ở trên cùng
+
+public class SceneChange : MonoBehaviour
+{
+    // Hàm này nhận vào tên của Scene mà a muốn chuyển tới
+    public void LoadMyScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // (Tùy chọn) Thêm hàm này nếu a muốn làm nút Thoát Game
+    public void QuitGame()
+    {
+        Debug.Log("Đã thoát game!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Hieu/Code_Hieu/TriggerShowUI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class TriggerShowUI : MonoBehaviour
+{
+    [Header("Giao diện (UI)")]
+    public GameObject uiToShow; // Kéo UI cần hiện vào đây
+
+    [Header("Cài đặt")]
+    public string targetTag = "Player"; // Tag của người chơi
+    public bool hideOnExit = true;      // Tích vào nếu muốn rời đi thì ẩn UI
+
+    void Start()
+    {
+        // Đảm bảo UI được ẩn đi khi mới vào game
+        if (uiToShow != null)
+        {
+            uiToShow.SetActive(false);
+        }
+    }
+
+    // Hàm này tự động chạy khi có vật thể chạm vào Trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra xem vật chạm vào có đúng là người chơi (mang tag Player) không
+        if (other.CompareTag(targetTag))
+        {
+            if (uiToShow != null)
+            {
+                uiToShow.SetActive(true);
+            }
+        }
+    }
+
+    // Hàm này tự động chạy khi vật thể đi ra khỏi Trigger
+    private void OnTriggerExit(Collider other)
+    {
+        // Nếu bật chế độ ẩn khi rời đi và đúng là người chơi vừa đi ra
+        if (hideOnExit && other.CompareTag(targetTag))
+        {
+            if (uiToShow != null)
+            {
+                uiToShow.SetActive(false);
+            }
+        }
+    }
+}
